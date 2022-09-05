827bfd78e77500615c703bb2f3b7d67d4605ae71
Elena <p0zhar@yandex.ru>
2022-09-05 15:37:17 +0400

    DZ64

---
+++ DZ64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+Console.WriteLine("Программа выводит все натуральные числа в промежутке от M до N.");
+Console.WriteLine();
+
+void PrintNaturalNumber(int m, int n) 
+{ 
+    if (m == n) Console.Write($"{n} "); 
+    else 
+    { 
+        Console.Write($"{m} "); 
+        PrintNaturalNumber(m + 1, n); 
+    } 
+} 
+
